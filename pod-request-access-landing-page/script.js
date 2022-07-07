4760091b41ab1727ed3051330c429acf6a8e2122
alexander215 <alexander.hoskinson@colorado.edu>
2022-07-07 12:31:19 -0600

    Update email validation to have different message for no email vs invalid format

--- pod-request-access-landing-page/script.js
+++ pod-request-access-landing-page/script.js
@@ -2,13 +2,14 @@ const form = document.querySelector('.input-email-form__submit-button');
 const emailAddress = document.querySelector('.input-email-form__email-input');
 const errorMessage = document.querySelector('.input-email-form__error-message');
 const inputContainer = document.querySelector('.input-email-form');
+let errorMessageToDisplay= "Oops! Please check your email.";
 
 
 const submitForm = (e) => {
   if (!validateEmail(emailAddress.value)){
     e.preventDefault();
     console.log(emailAddress.value);
-    errorMessage.innerText = "Oops! Please check your email."
+    errorMessage.innerText = errorMessageToDisplay;
   } else {
     errorMessage.style.display = "none";
     inputContainer.innerText = `Thanks! ${emailAddress.value} has been added.`
@@ -16,7 +17,12 @@ const submitForm = (e) => {
 };
 
 const validateEmail = (email) => {
-  return email.includes('@') && email.includes('.');
+  let validateStatus = email.includes('@') && email.includes('.');
+  if (email === '') {
+    errorMessageToDisplay = "Oops! Please add your email."
+  }
+
+  return validateStatus;
 };
 
 form.addEventListener('click', submitForm);
